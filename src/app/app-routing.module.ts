77ddb716ce625bf21d536749328fc3e6f7bf582f
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 09:31:30 -0500

    Agregando componente de dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core'
-import { Router, RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router'
 import { HeroesComponent } from './featured/components/heroes/heroes.component'
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component'
 import { HeroDetailComponent } from './featured/components/hero-detail/hero-detail.component'
 import { HeroesComponent } from './featured/components/heroes/heroes.component'
 import { MessagesComponent } from './featured/components/messages/messages.component'
 import { HeroService } from './featured/shared/hero.service'
-import { MessagesService } from './featured/shared/messages.service'
+import { MessagesService } from './featured/shared/messages.service';
+import { DashboardComponent } from './featured/components/dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
@@ -15,8 +18,9 @@ import { MessagesService } from './featured/shared/messages.service'
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [HeroService, MessagesService],
   bootstrap: [AppComponent],
 })
