3de1198629394b9aea076bd881b44a498f7b9944
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 08:04:34 -0500

    Creando componente de mesnajes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { HeroDetailComponent } from './featured/components/hero-detail/hero-detail.component'
 import { HeroesComponent } from './featured/components/heroes/heroes.component'
-import { HeroService } from './shared/hero.service'
+import { HeroService } from './shared/hero.service';
+import { MessagesComponent } from './featured/components/messages/messages.component'
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [HeroService],
   bootstrap: [AppComponent],
