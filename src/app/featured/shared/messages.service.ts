1b4684eee30f3217e279bf3edb954953cb58c552
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 08:11:55 -0500

    Generando metodos en el servicio

--- src/app/featured/shared/messages.service.ts
+++ src/app/featured/shared/messages.service.ts
@@ -2,5 +2,15 @@ import { Injectable } from '@angular/core'
 
 @Injectable()
 export class MessagesService {
+  public messages: string[] = []
+
   constructor() {}
+
+  public add(messages: string): void {
+    this.messages.push(messages)
+  }
+
+  public clear(): void {
+    this.messages = []
+  }
 }
