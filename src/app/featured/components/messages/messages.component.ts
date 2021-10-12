ef318dad09f401069febba40834c52033e491673
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 08:39:51 -0500

    Mostrar el mensaje de HeroService

--- src/app/featured/components/messages/messages.component.ts
+++ src/app/featured/components/messages/messages.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { MessagesService } from '../../shared/messages.service'
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  constructor(protected service: MessagesService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public length(): number {
+    return this.service.messages.length
   }
 
+  public clear(): void {
+    return this.service.clear()
+  }
+
+  public messages(): string[] {
+    return this.service.messages
+  }
 }
