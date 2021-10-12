8267aa2b5a32efbe3b91813f0b3f66f656c672fe
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 09:38:07 -0500

    Creando componente

--- src/app/featured/components/dashboard/dashboard.component.ts
+++ src/app/featured/components/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HeroService } from '../../shared/hero.service'
+import { Hero } from '../heroes/shared/Hero'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  public heroes: Hero[] = []
 
-  constructor() { }
+  constructor(protected service: HeroService) {}
 
   ngOnInit(): void {
+    this.getHeroes()
   }
-
+  getHeroes(): void {
+    this.service.getHeroes().subscribe((value) => (this.heroes = value))
+  }
 }
