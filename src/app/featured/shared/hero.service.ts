c513a320b64ac7243b3d8b76e0a3fcd09a2498d8
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 08:15:36 -0500

    Injectando MessagesService dentro de HeroService

--- src/app/featured/shared/hero.service.ts
+++ src/app/featured/shared/hero.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core'
 import { Observable, of } from 'rxjs'
 import { HEROES } from 'src/app/shared/mock-heroes'
 import { Hero } from '../components/heroes/shared/Hero'
+import { MessagesService } from './messages.service'
 
 @Injectable()
 export class HeroService {
-  constructor() {}
+  constructor(protected messages: MessagesService) {}
 
   public getHeroes(): Observable<Hero[]> {
+    this.messages.add('HeroService: fetched heroes.')
     return of(HEROES)
   }
 }
