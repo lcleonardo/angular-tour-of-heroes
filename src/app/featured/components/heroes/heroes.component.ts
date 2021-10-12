12dc07245986fe09f91997e1efca84a451539e78
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 10:15:25 -0500

    Limpiando codigo muerto

--- src/app/featured/components/heroes/heroes.component.ts
+++ src/app/featured/components/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core'
 import { HeroService } from '../../shared/hero.service'
-import { MessagesService } from '../../shared/messages.service'
 import { Hero } from './shared/Hero'
 
 @Component({
@@ -9,22 +8,12 @@ import { Hero } from './shared/Hero'
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero
-
   heroes: Hero[] = []
 
-  constructor(
-    protected heroService: HeroService,
-    protected messageService: MessagesService,
-  ) {}
+  constructor(protected heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes()
-  }
-
-  public onSelect(hero: Hero): void {
-    this.selectedHero = hero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   public getHeroes(): void {
