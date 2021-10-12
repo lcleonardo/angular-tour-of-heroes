033544989dbda5e3833b6abadd16178f480a9bb1
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 09:59:13 -0500

    Agregando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { DashboardComponent } from './featured/components/dashboard/dashboard.component'
 import { HeroesComponent } from './featured/components/heroes/heroes.component'
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
     path: 'heroes',
     component: HeroesComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
   },
 ]
 
