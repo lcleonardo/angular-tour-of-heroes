aee84cde27d2c69e5b380320fc4129439294ba8e
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 07:29:35 -0500

    creacion de hero detail componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { HeroesComponent } from './heroes/heroes.component'
+import { HeroDetailComponent } from './featured/components/hero-detail/hero-detail.component'
+import { HeroesComponent } from './featured/heroes/heroes.component'
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/featured/heroes/heroes.component.ts
+++ src/app/featured/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { HEROES } from '../shared/mock-heroes'
+import { HEROES } from 'src/app/shared/mock-heroes'
 import { Hero } from './shared/Hero'
 
 @Component({
