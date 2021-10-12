945d58e58c2418bb9025ef37978a5fce627ea092
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-10-12 08:08:55 -0500

    Arreglando imports

--- src/app/featured/components/heroes/heroes.component.ts
+++ src/app/featured/components/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { HeroService } from 'src/app/shared/hero.service'
+import { HeroService } from '../../shared/hero.service'
 import { Hero } from './shared/Hero'
 
 @Component({
